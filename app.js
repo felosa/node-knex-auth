bb7b6c73172956326d154129ee6c59384a26b64d
felosa <flo@archerknight.com>
2020-03-07 13:11:23 +0000

    refresh

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const session = require('express-session');
 const logger = require("morgan");
+const cors = require('cors');
+
+const whitelist = [
+  'http://localhost:3000'
+];
+const corsOptions = {
+  origin: function(origin, callback){
+      var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+      callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 const plans = require("./api/plans");
 const trips = require("./api/trips");
